7985dc6331faf93b89fbedec48188ad7ea9bfd4a
jovimo3 <jovimo3@aumni.uv.es>
2025-02-07 19:48:27 +0100

    Perímetro rectángulo y cambio PI

--- Perim.R
+++ Perim.R
@@ -3,5 +3,9 @@ PerimetroCuadrado <- function(L){
 }
 
 PerimCirculo <- function(R){
-  return(2*pi*R)
+  return(2*3.14159*R)
+}
+
+PerimRectangulo <- function(base, altura) {
+  return(2 * (base + altura))
 }
